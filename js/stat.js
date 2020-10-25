73818503a9babfd20500f434eae2ab1f65d01c62
Евгений <evgeniy_karpov91@inbox.ru>
2020-10-25 18:39:27 +0300

    облако статистики, правки

--- js/stat.js
+++ js/stat.js
@@ -15,7 +15,7 @@ const renderCloud = function (ctx, x, y, color) {
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-let getMaxElement = function (arr) {
+const getMaxElement = function (arr) {
   let maxElement = arr[0];
 
   for (let i = 1; i < arr.length; i++) {
@@ -31,21 +31,35 @@ const randomInt = function (max) {
   return Math.floor(Math.random() * Math.floor(max));
 };
 
+const randomBlueColor = function () {
+  return `hsl(230, ${randomInt(100)}%, 50%)`;
+};
+
 window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
   ctx.font = "16px PT Mono";
 
-  let maxTime = getMaxElement(times);
+  const maxTime = getMaxElement(times);
+
+  const fillScore = function (x, y, i) {
+    if (players[i] === `Вы`) {
+      ctx.fillStyle = `rgba(255, 0, 0, 1)`;
+    } else {
+      ctx.fillStyle = randomBlueColor();
+    }
+    ctx.fillRect(x, y - BAR_WIDTH + GAP, BAR_WIDTH, ((BAR_HEIGHT * times[i]) / maxTime) * -1);
+    ctx.fillStyle = '#000';
+    ctx.textAlign = 'center';
+    ctx.fillText(Math.floor(times[i]), x + BAR_WIDTH / 2, y - BAR_WIDTH - (BAR_HEIGHT * times[i]) / maxTime);
+    ctx.fillText(players[i], x + BAR_WIDTH / 2, y - GAP);
+  };
 
   for (let i = 0; i < players.length; i++) {
-    ctx.fillStyle = (players[i] === `Вы`) ? `rgba(255, 0, 0, 1)` : `hsl(230, ${randomInt(100)}%, 50%)`;
-    ctx.fillRect(CLOUD_X + BAR_WIDTH + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - BAR_WIDTH + GAP, BAR_WIDTH, ((BAR_HEIGHT * times[i]) / maxTime) * -1);
-    ctx.fillStyle = '#000';
-    ctx.fillText(Math.floor(times[i]), CLOUD_X + BAR_WIDTH + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - BAR_WIDTH - (BAR_HEIGHT * times[i]) / maxTime);
-    ctx.fillText(players[i], CLOUD_X + BAR_WIDTH + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT - GAP);
+    fillScore(CLOUD_X + BAR_WIDTH + (BAR_WIDTH + BAR_GAP) * i, CLOUD_HEIGHT, i);
   }
+  ctx.textAlign = 'left';
   ctx.fillText('Ура, вы победили!', 120, 40);
   ctx.fillText('Список результатов:', 120, 60);
 };
